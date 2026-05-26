a5cdb207d1e84f7f1d2c7ae8e51c5c68f1e779fc
JosFost125 <84982440+JosFost125@users.noreply.github.com>
2026-05-26 00:24:14 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Seleccionamos el botón de hamburguesa y el contenedor de los enlaces
-const botonMenu = document.querySelector('.menu-movil');
-const enlacesNav = document.querySelector('.nav-links');
-
-// Escuchamos el clic en el botón para activar o desactivar el menú
-botonMenu.addEventListener('click', () => {
-    enlacesNav.classList.toggle('activo');
-});
